188d6cfa40827190bc281c071db0697bc1b215b2
Ben Hencke <brainstar@gmail.com>
2017-11-12 18:23:46 -0800

    cleanup, fix left eye shift, rick

--- apptemplate.c
+++ apptemplate.c
@@ -18,24 +18,70 @@
 #define s_run 1
 #define s_freeze 2
 
+#define screenWidth 128
+#define screenHeight 112
+#define mapWidth 24
+#define mapHeight 24
+
+#define texHeight 32
+#define texWidth 32
+
+#define TEXTURE_MEM_OFFSET (128*128)
+#define TEXTURE_SIZE (texWidth*texHeight)
+
+#define TEXTURE(t,x,y) cambuffer[TEXTURE_MEM_OFFSET + TEXTURE_SIZE * t + y*texWidth + x]
+
+
+
 const uint8_t sinlut[256] = {
-    0x80,0x83,0x86,0x89,0x8c,0x8f,0x92,0x95,0x98,0x9b,0x9e,0xa2,0xa5,0xa7,0xaa,0xad,
-    0xb0,0xb3,0xb6,0xb9,0xbc,0xbe,0xc1,0xc4,0xc6,0xc9,0xcb,0xce,0xd0,0xd3,0xd5,0xd7,
-    0xda,0xdc,0xde,0xe0,0xe2,0xe4,0xe6,0xe8,0xea,0xeb,0xed,0xee,0xf0,0xf1,0xf3,0xf4,
-    0xf5,0xf6,0xf8,0xf9,0xfa,0xfa,0xfb,0xfc,0xfd,0xfd,0xfe,0xfe,0xfe,0xff,0xff,0xff,
-    0xff,0xff,0xff,0xff,0xfe,0xfe,0xfe,0xfd,0xfd,0xfc,0xfb,0xfa,0xfa,0xf9,0xf8,0xf6,
-    0xf5,0xf4,0xf3,0xf1,0xf0,0xee,0xed,0xeb,0xea,0xe8,0xe6,0xe4,0xe2,0xe0,0xde,0xdc,
-    0xda,0xd7,0xd5,0xd3,0xd0,0xce,0xcb,0xc9,0xc6,0xc4,0xc1,0xbe,0xbc,0xb9,0xb6,0xb3,
-    0xb0,0xad,0xaa,0xa7,0xa5,0xa2,0x9e,0x9b,0x98,0x95,0x92,0x8f,0x8c,0x89,0x86,0x83,
-    0x80,0x7c,0x79,0x76,0x73,0x70,0x6d,0x6a,0x67,0x64,0x61,0x5d,0x5a,0x58,0x55,0x52,
-    0x4f,0x4c,0x49,0x46,0x43,0x41,0x3e,0x3b,0x39,0x36,0x34,0x31,0x2f,0x2c,0x2a,0x28,
-    0x25,0x23,0x21,0x1f,0x1d,0x1b,0x19,0x17,0x15,0x14,0x12,0x11,0xf,0xe,0xc,0xb,
-    0xa,0x9,0x7,0x6,0x5,0x5,0x4,0x3,0x2,0x2,0x1,0x1,0x1,0x0,0x0,0x0,
-    0x0,0x0,0x0,0x0,0x1,0x1,0x1,0x2,0x2,0x3,0x4,0x5,0x5,0x6,0x7,0x9,
-    0xa,0xb,0xc,0xe,0xf,0x11,0x12,0x14,0x15,0x17,0x19,0x1b,0x1d,0x1f,0x21,0x23,
-    0x25,0x28,0x2a,0x2c,0x2f,0x31,0x34,0x36,0x39,0x3b,0x3e,0x41,0x43,0x46,0x49,0x4c,
-    0x4f,0x52,0x55,0x58,0x5a,0x5d,0x61,0x64,0x67,0x6a,0x6d,0x70,0x73,0x76,0x79,0x7c,
+    0x80, 0x83, 0x86, 0x89, 0x8c, 0x8f, 0x92, 0x95, 0x98, 0x9b, 0x9e, 0xa2, 0xa5, 0xa7, 0xaa, 0xad,
+    0xb0, 0xb3, 0xb6, 0xb9, 0xbc, 0xbe, 0xc1, 0xc4, 0xc6, 0xc9, 0xcb, 0xce, 0xd0, 0xd3, 0xd5, 0xd7,
+    0xda, 0xdc, 0xde, 0xe0, 0xe2, 0xe4, 0xe6, 0xe8, 0xea, 0xeb, 0xed, 0xee, 0xf0, 0xf1, 0xf3, 0xf4,
+    0xf5, 0xf6, 0xf8, 0xf9, 0xfa, 0xfa, 0xfb, 0xfc, 0xfd, 0xfd, 0xfe, 0xfe, 0xfe, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfc, 0xfb, 0xfa, 0xfa, 0xf9, 0xf8, 0xf6,
+    0xf5, 0xf4, 0xf3, 0xf1, 0xf0, 0xee, 0xed, 0xeb, 0xea, 0xe8, 0xe6, 0xe4, 0xe2, 0xe0, 0xde, 0xdc,
+    0xda, 0xd7, 0xd5, 0xd3, 0xd0, 0xce, 0xcb, 0xc9, 0xc6, 0xc4, 0xc1, 0xbe, 0xbc, 0xb9, 0xb6, 0xb3,
+    0xb0, 0xad, 0xaa, 0xa7, 0xa5, 0xa2, 0x9e, 0x9b, 0x98, 0x95, 0x92, 0x8f, 0x8c, 0x89, 0x86, 0x83,
+    0x80, 0x7c, 0x79, 0x76, 0x73, 0x70, 0x6d, 0x6a, 0x67, 0x64, 0x61, 0x5d, 0x5a, 0x58, 0x55, 0x52,
+    0x4f, 0x4c, 0x49, 0x46, 0x43, 0x41, 0x3e, 0x3b, 0x39, 0x36, 0x34, 0x31, 0x2f, 0x2c, 0x2a, 0x28,
+    0x25, 0x23, 0x21, 0x1f, 0x1d, 0x1b, 0x19, 0x17, 0x15, 0x14, 0x12, 0x11, 0xf, 0xe, 0xc, 0xb,
+    0xa, 0x9, 0x7, 0x6, 0x5, 0x5, 0x4, 0x3, 0x2, 0x2, 0x1, 0x1, 0x1, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x1, 0x1, 0x1, 0x2, 0x2, 0x3, 0x4, 0x5, 0x5, 0x6, 0x7, 0x9,
+    0xa, 0xb, 0xc, 0xe, 0xf, 0x11, 0x12, 0x14, 0x15, 0x17, 0x19, 0x1b, 0x1d, 0x1f, 0x21, 0x23,
+    0x25, 0x28, 0x2a, 0x2c, 0x2f, 0x31, 0x34, 0x36, 0x39, 0x3b, 0x3e, 0x41, 0x43, 0x46, 0x49, 0x4c,
+    0x4f, 0x52, 0x55, 0x58, 0x5a, 0x5d, 0x61, 0x64, 0x67, 0x6a, 0x6d, 0x70, 0x73, 0x76, 0x79, 0x7c,
 };
+
+
+int worldMap[mapWidth][mapHeight] = {
+    {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 7, 7, 7, 7, 7, 7, 7},
+    {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 4, 0, 0, 0, 0, 5, 1, 2, 5, 5, 6, 6, 5, 5, 7, 7, 0, 7, 7, 7, 7, 7},
+    {4, 0, 5, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+    {4, 0, 6, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 4},
+    {4, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 1},
+    {4, 0, 4, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 4},
+    {4, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+    {4, 0, 0, 0, 0, 0, 0, 5, 3, 5, 4, 0, 5, 6, 5, 5, 7, 7, 7, 7, 7, 7, 7, 1},
+    {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 1, 2, 3, 4, 5, 6, 7, 6},
+    {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+    {6, 6, 6, 6, 6, 6, 0, 6, 1, 2, 3, 0, 4, 5, 6, 7, 4, 6, 3, 1, 6, 4, 4, 6},
+    {8, 8, 8, 8, 8, 1, 0, 1, 8, 8, 6, 0, 6, 2, 3, 4, 5, 6, 7, 2, 3, 3, 3, 3},
+    {8, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+    {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+    {8, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+    {8, 0, 6, 0, 6, 0, 0, 0, 0, 8, 6, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2},
+    {8, 0, 0, 5, 0, 0, 0, 0, 0, 8, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+    {8, 0, 6, 0, 6, 0, 0, 0, 0, 8, 6, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+    {8, 0, 0, 0, 0, 0, 0, 0, 0, 8, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+    {8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3}
+};
+
+
+extern const unsigned char hires_texture[];
 
 void draw();
 void readKeys();
@@ -57,6 +103,7 @@ enum {
 double posX = 22.0, posY = 11.5; //x and y start position
 double dirX = -1.0, dirY = 0.0; //initial direction vector
 double planeX = 0.0, planeY = 0.66; //the 2d raycaster version of camera plane
+
 
 double paletteOffset = 0;
 int rxError = 0;
@@ -80,13 +127,13 @@ void printMode() {
 }
 
 int u2rxbyte() {
-    
+
     T1CON = 0b1000000000000000;
     T1CONbits.TCKPS0 = 1;
     PR1 = 0xffff;
     TMR1 = 0;
     IFS0CLR = _IFS0_T1IF_MASK;
-    
+
     while (!U2STAbits.URXDA) {
         if (U2STAbits.OERR) {
             U2STAbits.OERR = 0;
@@ -140,14 +187,13 @@ txState() {
     rxError = 0;
     if (u2rxbyte() != 1) { //wait for other side to ack
         rxError = 1;
-//        delayus(10000); //delay long enough for rx to timeout and reset
+        //        delayus(10000); //delay long enough for rx to timeout and reset
     }
-    
+
 }
 
 
-#define eyeShift -0.2
-
+#define eyeShift .1
 
 rxState() {
     rxError = 1;
@@ -155,6 +201,7 @@ rxState() {
     double rposX = 22.0, rposY = 11.5; //x and y start position
     double rdirX = -1.0, rdirY = 0.0; //initial direction vector
     double rplaneX = 0.0, rplaneY = 0.66; //the 2d raycaster version of camera plane
+    double oldDirX, oldPlaneX;
 
     if (u2rxbyte() != 0) return; //frame start
     if ((rposX = receiveDouble()) == DBL_MIN) return;
@@ -166,20 +213,27 @@ rxState() {
     if ((paletteOffset = receiveDouble()) == DBL_MIN) return;
     if (u2rxbyte() != 0xff) return; //frame end
 
+
     posX = rposX;
     posY = rposY;
     dirX = rdirX;
     dirY = rdirY;
     planeX = rplaneX;
     planeY = rplaneY;
-    
-    
+
     //shift eye position
-//    rposX = rposX + rdirX * eyeShift;
-//    rposY = rposY + rdirY * eyeShift;
+    //    rposX = rposX + rdirX * eyeShift;
+    //    rposY = rposY + rdirY * eyeShift;
     //(x,y) rotated left 90 deg is (-y, x) . pretend that we faced left, moved a bit, then restored direction
     rposX = rposX + -rdirY * eyeShift;
     rposY = rposY + rdirX * eyeShift;
+    
+    if (worldMap[(int) rposX][(int) rposY] == 0) {
+        posX = rposX;
+        posY = rposY;
+    }
+    
+ 
 
     rxError = 0;
     u2txbyte(1); //send ack    
@@ -190,8 +244,9 @@ char* demoapp(unsigned int action) {
     unsigned int x, y, i;
 
     switch (action) {
-        case act_name: return ("TEST_APP");
-        case act_help: return ("line1\nline2\nline3");
+        case act_name: return ("Rick Caster");
+        case act_help: return ("Nvr gona giv u up, nvr gona let u dwn,nvr gona rn arnd & dsrt u");
+       
         case act_init:
             // add any code here that needs to run once at powerup - e.g. hardware detection/initialisation      
             return (0);
@@ -211,15 +266,15 @@ char* demoapp(unsigned int action) {
 
     // do anything that needs to be faster than tick here.
 
-    
-        if (!tick) return (0);
+
+    //    if (!tick) return (0);
     timer += tick;
 
     if (timer >= fpsTimer + 50) {
-        fps = (50 * frames) / (timer - fpsTimer);
+        fps = ((1000000 / ticktime) * frames) / (timer - fpsTimer);
         fpsTimer = timer;
         frames = 0;
-//        makepalette(timer);
+        //        makepalette(timer);
     }
 
     switch (state) {
@@ -308,64 +363,25 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 
-#define screenWidth 128
-#define screenHeight 112
-#define mapWidth 24
-#define mapHeight 24
-
-#define texHeight 32
-#define texWidth 32
-
-#define TEXTURE_MEM_OFFSET (128*128)
-#define TEXTURE_SIZE (texWidth*texHeight)
-
-#define TEXTURE(t,x,y) cambuffer[TEXTURE_MEM_OFFSET + TEXTURE_SIZE * t + y*texWidth + x]
-
-
-int worldMap[mapWidth][mapHeight] = {
-    {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 7, 7, 7, 7, 7, 7, 7},
-    {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
-    {4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
-    {4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
-    {4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
-    {4, 0, 4, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 0, 7, 7, 7, 7, 7},
-    {4, 0, 5, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
-    {4, 0, 6, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
-    {4, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 1},
-    {4, 0, 8, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
-    {4, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
-    {4, 0, 0, 0, 0, 0, 0, 5, 3, 5, 4, 0, 5, 6, 5, 5, 7, 7, 7, 7, 7, 7, 7, 1},
-    {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 1, 2, 3, 4, 5, 6, 7, 6},
-    {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
-    {6, 6, 6, 6, 6, 6, 0, 6, 1, 2, 3, 0, 4, 5, 6, 7, 8, 6, 6, 6, 6, 6, 8, 6},
-    {4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 6, 0, 6, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3},
-    {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
-    {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
-    {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
-    {4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2},
-    {4, 0, 0, 5, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
-    {4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
-    {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
-    {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3}
-};
-
-
 
 double time = 0; //time of current frame
 double oldTime = 0; //time of previous frame
 
-
 void makepalette(int offset) {
     unsigned int i, d;
-    uint8_t r, g, b;
+    int r, g, b;
     for (i = 0; i != 256; i++) {
         d = i + offset;
-        r = (sinlut[d & 0xff] *i)>>8;
-        g = (sinlut[(d+85) & 0xff] *i)>>8;
-        b = (sinlut[(d+170) & 0xff] *i)>>8;
-        palette[i] = rgbto16(r, b, g);
+        r = sinlut[d & 0xff];
+        r = (r * r * i) >> 15;
+        g = sinlut[(d + 85) & 0xff];
+        g = (g * g * i) >> 15;
+        b = sinlut[(d + 170) & 0xff];
+        b = (b * b * i) >> 15;
+        palette[i] = rgbto16(min(r, 255), min(g, 255), min(b, 255));
     }
     palette[0] = 0;
+    palette[255] = 0xffff;
 }
 
 void generateTextures() {
@@ -373,42 +389,24 @@ void generateTextures() {
     for (x = 0; x < texWidth; x++) {
         for (y = 0; y < texHeight; y++) {
 
-            TEXTURE(0, x, y) = ((x + y)&0x7) * 8;
-            TEXTURE(1, x, y) = abs(x - y)*4;
+            TEXTURE(0, x, y) = 64 + (x % 8) * (y % 12) + abs(sin((x^y) / 64.0)*64);
+            TEXTURE(1, x, y) = ((texWidth - x)*(texWidth - y));
             TEXTURE(2, x, y) = abs(y - x)*4;
-            TEXTURE(3, x, y) = (x + y)*4;
+            TEXTURE(3, x, y) = ((texWidth - x) ^ y) * 4 + abs(sin(y / 32.0)*128);
             TEXTURE(4, x, y) = x*y;
             TEXTURE(5, x, y) = abs(sin((x - texWidth) / 6.0) * cos((y - texHeight) / 6.0) * 255);
             TEXTURE(6, x, y) = (x ^ y) * 4;
-            TEXTURE(7, x, y) = 128 * (x % 8 && y % 8);
+            TEXTURE(7, x, y) = 192 * (x % 8 && y % 8);
             TEXTURE(8, x, y) = (x - texWidth)*4;
 
         }
     }
     makepalette(paletteOffset);
 
-    ////generate some textures
-    ////  for(int x = 0; x < texWidth; x++)
-    ////  for(int y = 0; y < texHeight; y++)
-    ////  {
-    ////    int xorcolor = (x * 256 / texWidth) ^ (y * 256 / texHeight);
-    ////    //int xcolor = x * 256 / texWidth;
-    ////    int ycolor = y * 256 / texHeight;
-    ////    int xycolor = y * 128 / texHeight + x * 128 / texWidth;
-    ////    texture[0][texWidth * y + x] = 65536 * 254 * (x != y && x != texWidth - y); //flat red texture with black cross
-    ////    texture[1][texWidth * y + x] = xycolor + 256 * xycolor + 65536 * xycolor; //sloped greyscale
-    ////    texture[2][texWidth * y + x] = 256 * xycolor + 65536 * xycolor; //sloped yellow gradient
-    ////    texture[3][texWidth * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; //xor greyscale
-    ////    texture[4][texWidth * y + x] = 256 * xorcolor; //xor green
-    ////    texture[5][texWidth * y + x] = 65536 * 192 * (x % 16 && y % 16); //red bricks
-    ////    texture[6][texWidth * y + x] = 65536 * ycolor; //red gradient
-    ////    texture[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
-    ////  }
-    //
 }
 
 void draw() {
-    int x, y, mapX, mapY, stepX, stepY, hit, side, lineHeight, drawStart, drawEnd, texNum, texX, d, texY, cOffset;
+    int x, y, mapX, mapY, stepX, stepY, hit, side, lineHeight, drawStart, drawEnd, texNum, texX, d, texY, cOffset, tw, th;
     double cameraX, rayPosX, rayPosY, rayDirX, rayDirY, sideDistX, sideDistY, deltaDistX, deltaDistY, perpWallDist, wallX, frameTime, moveSpeed, rotSpeed, oldPlaneX, oldDirX;
 
     for (x = 0; x < screenWidth; x++) for (y = 0; y < screenHeight; y++) PIXEL(x, y) = 0; //clear the buffer instead of cls()    
@@ -486,6 +484,14 @@ void draw() {
 
         //texturing calculations
         texNum = worldMap[mapX][mapY] - 1; //1 subtracted from it so that texture 0 can be used!
+        
+        if (texNum == 7) {
+            tw = 128;
+            th = 128;
+        } else {
+            tw = texWidth;
+            th = texHeight;
+        }
 
         //calculate value of wallX
         //where exactly the wall was hit
@@ -495,51 +501,50 @@ void draw() {
 
 
         //x coordinate on the texture
-        texX = wallX * texWidth;
-        if (side == 0 && rayDirX > 0) texX = texWidth - texX - 1;
-        if (side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
-        
-        
+        texX = wallX * tw;
+        if (side == 0 && rayDirX > 0) texX = tw - texX - 1;
+        if (side == 1 && rayDirY < 0) texX = tw - texX - 1;
 
         for (y = drawStart; y < drawEnd; y++) {
-            d = y * 256 - screenHeight * 128 + lineHeight * 128; //256 and 128 factors to avoid floats
-            texY = ((d * texHeight) / lineHeight) / 256;
+            d = y * 512 - screenHeight * 256 + lineHeight * 256; //256 and 128 factors to avoid floats
+            texY = ((d * th) / lineHeight) / 512;
 
-            //Uint32 color = texture[texNum][texHeight * texY + texX];
-            uint8_t color = TEXTURE(texNum, texX, texY);
-            //            make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
-            //            if (side == 1) color = (color >> 1);
+            uint8_t color;
+            if (texNum == 7) {
+                color = hires_texture[texY*128 + texX];
+            } else {
+                color = TEXTURE(texNum, texX, texY);
+            }
+
+
             //flip horizontal, vertical flip done during dispimage
             if (syncMode == LEFTSIDE)
-                PIXEL((screenWidth - x), y) = color;
-            else 
+                PIXEL((screenWidth - x - 1), y) = color;
+            else
                 PIXEL(x, y) = color;
         }
     }
+    
+    
+    //draw the map
+    for (x = 0; x < mapWidth; x++) {
+        for (y = 0; y < mapHeight; y++) {
+            uint8_t color = 0;
+            if (worldMap[x][y])
+                color = 255;
+            if (x == ((int)posX) && (y == (int) posY))
+                color = (frames & 0x2) ? 1: 255;
+            
+            if (!color)
+                continue;
 
-    //    drawBuffer(buffer[0]);
-
-    //    //timing for input and FPS counter
-    //    oldTime = time;
-    //    time = getTicks();
-    //    frameTime = (time - oldTime) / 1000.0; //frametime is the time this frame has taken, in seconds
-    //    print(1.0 / frameTime); //FPS counter
-    //    redraw();
-
-
-    //    drawBuffer(buffer[0]);
-    //    for(int x = 0; x < w; x++) for(int y = 0; y < h; y++) buffer[y][x] = 0; //clear the buffer instead of cls()
-    //    //timing for input and FPS counter
-    //    oldTime = time;
-    //    time = getTicks();
-    //    double frameTime = (time - oldTime) / 1000.0; //frametime is the time this frame has taken, in seconds
-    //    print(1.0 / frameTime); //FPS counter
-    //    redraw();
-    //
-    //    //speed modifiers
-    //    double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
-    //    double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
-    //
+            if (syncMode == LEFTSIDE)
+                PIXEL((screenWidth - (mapWidth-x) - 4), y) = color;
+            else
+                PIXEL(mapWidth-x, y) = color;
+        }
+    }
+    
 }
 
 #define deadzone 500
@@ -599,11 +604,6 @@ void readKeys() {
         planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
 
     }
-    
-//    if (abs(accy) < 16000) {
-//        paletteOffset += accy/1000.0;
-//        makepalette(paletteOffset);
-//    }
 
     //    //rotate to the right
     //    if (butpress & but2) {
